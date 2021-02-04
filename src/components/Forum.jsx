83996991e246b5f988e23c27a7899e1fae022c02
Francis <francisventura1@hotmail.com>
2021-02-04 16:22:17 -0500

    more forms

--- src/components/Forum.jsx
+++ src/components/Forum.jsx
@@ -23,14 +23,13 @@ export class Forum extends React.Component {
           this.setState({loading: false, foruminfo: data})
           console.log(this.state.foruminfo);
         
-        
     }
 
     handleCommentSubmit(event){
       event.preventDefault();
-      console.log(event.target);
       console.log(this.state.formUserInput);
       console.log(this.state.formCommentInput);
+      document.getElementById('forum-form').reset();
     }
 
     handleInputChange(event){
@@ -47,7 +46,7 @@ export class Forum extends React.Component {
             <h1>Forum</h1>
            { this.state.loading ? <h2>Loading...</h2>: this.state.foruminfo.map((item) => <li key={item._id}>{item.user}&nbsp;{item.comment}</li>)}
             
-            <form onSubmit={this.handleCommentSubmit}>
+            <form id="forum-form" onSubmit={this.handleCommentSubmit}>
               <input type="text" name="formUserInput" id="userinput" onChange={this.handleInputChange} />
               <input type="text" name="formCommentInput" id="commentinput" onChange={this.handleInputChange} />
               <button type="submit">submit</button>
--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,12 +3,37 @@ import React from 'react';
 export class LogIn extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            username : '',
+            password : ''
+        }
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
+    handleFormChange(event){
+        var nam = event.target.name;
+        var val = event.target.value;
+        this.setState({
+            [nam]: val
+        });
+        
+    }
+
+    handleFormSubmit(event){
+        event.preventDefault();
+        
+        document.getElementById('login-form').reset();
+    }
     render(){
         return (<div id="login">
             <h1>Login</h1>
             
+            <form id="login-form" onSubmit={this.handleFormSubmit} > 
+                <input type="text" name="username" id="username" onChange={this.handleFormChange} />
+                <input type="password" name="password" id="password" onChange={this.handleFormChange} />
+                <button type="submit">submit</button>
+            </form>
         </div>);
     }
 }
